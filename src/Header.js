eb4e077d2a9d4368a028fad19d6d96e69f256b28
armirqurdina00 <armirqurdina@gmail.com>
2023-06-15 10:41:38 +0200

    Add icon to header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="header">
-            <h1><a href="/">The Weather</a></h1>
+            <h1><a href="/"><i class="fa-solid fa-cloud"></i> The Weather</a></h1>
         </div>
     )
 }
